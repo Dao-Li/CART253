5f9c4eb96de4eb91b3312696bf521928da7b8c03
Dao-Li <dao.li.lr@gmail.com>
2019-10-08 16:53:09 -0400

    P1: Change the player and prey images
    Player is now Gargamel and the prey is now a smurf

--- Exercices/Projet 01/js/script.js
+++ Exercices/Projet 01/js/script.js
@@ -5,10 +5,10 @@
 Game - Chaser
 Modified by Dao-Li Leboeuf Roy;
 
-A "simple" game of cat and mouse. The player is a circle and can move with keys,
-if they overlap the (randomly moving according to Noise perlin) prey they "eat it"
-by sucking out its life and adding it to their own. The player "dies" slowly
-over time so they have to keep eating to stay alive.
+A "simple" game of cat and mouse. The player (Gargamel) can move with keys
+to overlap the (randomly moving according to Noise perlin) smurf to catch it.
+Each time it catch one, it fuels his hope and gives him health. Gargamel "dies"
+slowly over time, because of losing hope, so he has to keep catching smurf to stay alive.
 
 Includes: Physics-based movement, keyboard controls, health/stamina,
 random movement (Perlin noise), screen wrap.
@@ -18,38 +18,47 @@ random movement (Perlin noise), screen wrap.
 // Track whether the game is over
 let gameOver = false;
 
-// Player position, size, velocity
-let playerX;
-let playerY;
-let playerRadius = 25;
-let playerVX = 0;
-let playerVY = 0;
-let playerMaxSpeed = 2;
-// Player health
-let playerHealth;
-let playerMaxHealth = 255;
-// Player fill color
-let playerFill = 50;
+// playerGargamel (player) position, size, velocity
+let playerGargamelX;
+let playerGargamelY;
+let playerGargamelscale = 0.3;
+let playerGargamelVX = 0;
+let playerGargamelVY = 0;
+let playerGargamelMaxSpeed = 3;
+// playerGargamel health
+let playerGargamelHealth;
+let playerGargamelMaxHealth = 255;
+// playerGargamel image to display
+let playerGargamelImage;
 
-// Prey position, size, velocity, noise time
-let preyX;
-let preyY;
-let preyRadius = 25;
-let preyVX;
-let preyVY;
-let preyMaxSpeed = 5;
-let preyNoiseTx;
-let preyNoiseTy;
-// Prey health
-let preyHealth;
-let preyMaxHealth = 100;
-// Prey fill color
-let preyFill = 200;
-
-// Amount of health obtained per frame of "eating" (overlapping) the prey
+// smurf (prey) position, size, velocity, noise time
+let smurfX;
+let smurfY;
+let smurfscale = 0.2;
+let smurfVX;
+let smurfVY;
+let smurfMaxSpeed = 5;
+let smurfNoiseTx;
+let smurfNoiseTy;
+// smurf health
+let smurfHealth;
+let smurfMaxHealth = 100;
+// smurf image to display
+let smurfImage;
+// Amount of health obtained per frame of "eating" (overlapping) the smurf
 let eatHealth = 10;
-// Number of prey eaten during the game (the "score")
-let preyEaten = 0;
+// Number of smurf eaten during the game (the "score")
+let smurfEaten = 0;
+
+//add preload function to load the assets images
+function preload() {
+  // Preload
+  // https://www.stickpng.com/fr/img/films/dessins-animes/les-schtroumpfs/schtroumpf-maladroit-se-cogne-a-un-rocher
+  smurfImage = loadImage("assets/images/smurf.png");
+  // https://pngimage.net/gargamel-png-5/
+  playerGargamelImage = loadImage("assets/images/gargamel.png");
+}
+
 
 // setup()
 //
@@ -60,36 +69,40 @@ function setup() {
   noStroke();
 
   // We're using simple functions to separate code out
-  setupPrey();
-  setupPlayer();
+  setupsmurf();
+  setupplayerGargamel();
 }
 
-// setupPrey()
+// setupsmurf()
 //
-// Initialises prey's position, velocity, and health
-function setupPrey() {
-  preyX = width / 5;
-  preyY = height / 2;
-  preyVX = -preyMaxSpeed;
-  preyVY = preyMaxSpeed;
-  preyHealth = preyMaxHealth;
-  preyNoiseTx = random(0,width);
-  preyNoiseTy = random(0, height);
+// Initialises smurf's position, velocity, and health
+function setupsmurf() {
+  smurfX = width / 5;
+  smurfY = height / 2;
+  smurfVX = -smurfMaxSpeed;
+  smurfVY = smurfMaxSpeed;
+  smurfHealth = smurfMaxHealth;
+  smurfNoiseTx = random(0,width);
+  smurfNoiseTy = random(0, height);
+  // Draw image from center
+  imageMode(CENTER)
 }
 
-// setupPlayer()
+// setupplayerGargamel()
 //
-// Initialises player position and health
-function setupPlayer() {
-  playerX = 4 * width / 5;
-  playerY = height / 2;
-  playerHealth = playerMaxHealth;
+// Initialises playerGargamel position and health
+function setupplayerGargamel() {
+  playerGargamelX = 4 * width / 5;
+  playerGargamelY = height / 2;
+  playerGargamelHealth = playerGargamelMaxHealth;
+  // Draw image from center
+  imageMode(CENTER)
 }
 
 // draw()
 //
 // While the game is active, checks input
-// updates positions of prey and player,
+// updates positions of smurf and playerGargamel,
 // checks health (dying), checks eating (overlaps)
 // displays the two agents.
 // When the game is over, shows the game over screen.
@@ -99,14 +112,14 @@ function draw() {
   if (!gameOver) {
     handleInput();
 
-    movePlayer();
-    movePrey();
+    moveplayerGargamel();
+    movesmurf();
 
     updateHealth();
     checkEating();
 
-    drawPrey();
-    drawPlayer();
+    drawsmurf();
+    drawplayerGargamel();
   }
   else {
     showGameOver();
@@ -115,71 +128,71 @@ function draw() {
 
 // handleInput()
 //
-// Checks arrow keys and adjusts player velocity accordingly
+// Checks arrow keys and adjusts playerGargamel velocity accordingly
 function handleInput() {
   // Check for horizontal movement
   if (keyIsDown(LEFT_ARROW)) {
-    playerVX = -playerMaxSpeed;
+    playerGargamelVX = -playerGargamelMaxSpeed;
   }
   else if (keyIsDown(RIGHT_ARROW)) {
-    playerVX = playerMaxSpeed;
+    playerGargamelVX = playerGargamelMaxSpeed;
   }
   else {
-    playerVX = 0;
+    playerGargamelVX = 0;
   }
 
   // Check for vertical movement
   if (keyIsDown(UP_ARROW)) {
-    playerVY = -playerMaxSpeed;
+    playerGargamelVY = -playerGargamelMaxSpeed;
   }
   else if (keyIsDown(DOWN_ARROW)) {
-    playerVY = playerMaxSpeed;
+    playerGargamelVY = playerGargamelMaxSpeed;
   }
   else {
-    playerVY = 0;
+    playerGargamelVY = 0;
   }
 }
 
-// movePlayer()
+// moveplayerGargamel()
 //
-// Updates player position based on velocity,
+// Updates playerGargamel position based on velocity,
 // wraps around the edges.
-function movePlayer() {
+function moveplayerGargamel() {
   // Update position
-  playerX = playerX + playerVX;
-  playerY = playerY + playerVY;
+  playerGargamelX = playerGargamelX + playerGargamelVX;
+  playerGargamelY = playerGargamelY + playerGargamelVY;
 
-  // Wrap when player goes off the canvas
-  if (playerX < 0) {
+  // Wrap when playerGargamel goes off the canvas
+  if (playerGargamelX < 0) {
     // Off the left side, so add the width to reset to the right
-    playerX = playerX + width;
+    playerGargamelX = playerGargamelX + width;
   }
-  else if (playerX > width) {
+  else if (playerGargamelX > width) {
     // Off the right side, so subtract the width to reset to the left
-    playerX = playerX - width;
+    playerGargamelX = playerGargamelX - width;
   }
 
-  if (playerY < 0) {
+  if (playerGargamelY < 0) {
     // Off the top, so add the height to reset to the bottom
-    playerY = playerY + height;
+    playerGargamelY = playerGargamelY + height;
   }
-  else if (playerY > height) {
+  else if (playerGargamelY > height) {
     // Off the bottom, so subtract the height to reset to the top
-    playerY = playerY - height;
+    playerGargamelY = playerGargamelY - height;
   }
 }
 
 // updateHealth()
 //
-// Reduce the player's health (happens every frame)
-// Check if the player is dead
+// Reduce the playerGargamel's health (happens every frame)
+// Check if the playerGargamel is dead
 function updateHealth() {
-  // Reduce player health
-  playerHealth = playerHealth - 0.5;
+  // Reduce playerGargamel health
+  playerGargamelHealth = playerGargamelHealth - 0.5;
   // Constrain the result to a sensible range
-  playerHealth = constrain(playerHealth, 0, playerMaxHealth);
-  // Check if the player is dead (0 health)
-  if (playerHealth === 0) {
+  playerGargamelHealth = constrain(playerGargamelHealth, 0, playerGargamelMaxHealth);
+  // Check if the playerGargamel is dead (0 health)
+  if (playerGargamelHealth === 0) {
     // If so, the game is over
     gameOver = true;
   }
@@ -187,87 +200,87 @@ function updateHealth() {
 
 // checkEating()
 //
-// Check if the player overlaps the prey and updates health of both
+// Check if the playerGargamel overlaps the smurf and updates health of both
 function checkEating() {
-  // Get distance of player to prey
-  let d = dist(playerX, playerY, preyX, preyY);
-  // Check if it's an overlap
-  if (d < playerRadius + preyRadius) {
-    // Increase the player health
-    playerHealth = playerHealth + eatHealth;
+  // Get distance of playerGargamel to smurf
+  let d = dist(playerGargamelX, playerGargamelY, smurfX, smurfY);
+  // Check if it's an overlap. Addition each image * their scale and divide them by two to
+  if (d < (playerGargamelImage.width * playerGargamelscale)/2 + (smurfImage.width * smurfscale)/2) {
+    // Increase the playerGargamel health
+    playerGargamelHealth = playerGargamelHealth + eatHealth;
     // Constrain to the possible range
-    playerHealth = constrain(playerHealth, 0, playerMaxHealth);
-    // Reduce the prey health
-    preyHealth = preyHealth - eatHealth;
+    playerGargamelHealth = constrain(playerGargamelHealth, 0, playerGargamelMaxHealth);
+    // Reduce the smurf health
+    smurfHealth = smurfHealth - eatHealth;
     // Constrain to the possible range
-    preyHealth = constrain(preyHealth, 0, preyMaxHealth);
+    smurfHealth = constrain(smurfHealth, 0, smurfMaxHealth);
 
-    // Check if the prey died (health 0)
-    if (preyHealth === 0) {
-      // Move the "new" prey to a random position
-      preyX = random(0, width);
-      preyY = random(0, height);
+    // Check if the smurf died (health 0)
+    if (smurfHealth === 0) {
+      // Move the "new" smurf to a random position
+      smurfX = random(0, width);
+      smurfY = random(0, height);
       // Give it full health
-      preyHealth = preyMaxHealth;
-      // Track how many prey were eaten
-      preyEaten = preyEaten + 1;
+      smurfHealth = smurfMaxHealth;
+      // Track how many smurf were eaten
+      smurfEaten = smurfEaten + 1;
     }
   }
 }
 
-// movePrey()
+// movesmurf()
 //
-// Moves the prey based on random velocity changes
-function movePrey() {
-  // Change the prey's velocity at random intervals
-  // random() will be < 0.02 2% of the time, so the prey
+// Moves the smurf based on random velocity changes
+function movesmurf() {
+  // Change the smurf's velocity at random intervals
+  // random() will be < 0.02 2% of the time, so the smurf
   // will change direction on 2% of frames
   if (random() < 0.02) {
     // Set velocity based on random values to get a new direction
     // and speed of movement
     //
     // Use map() to convert from the 0-1 range of the random() function
-    // to the appropriate range of velocities for the prey
-    preyVX = map(noise(preyNoiseTx), 0, 1, -preyMaxSpeed, preyMaxSpeed);
-    preyVY = map(noise(preyNoiseTy), 0, 1, -preyMaxSpeed, preyMaxSpeed);
-    preyNoiseTx += 5;
-    preyNoiseTy += 5;
+    // to the appropriate range of velocities for the smurf
+    smurfVX = map(noise(smurfNoiseTx), 0, 1, -smurfMaxSpeed, smurfMaxSpeed);
+    smurfVY = map(noise(smurfNoiseTy), 0, 1, -smurfMaxSpeed, smurfMaxSpeed);
+    smurfNoiseTx += 5;
+    smurfNoiseTy += 5;
   }
 
-  // Update prey position based on velocity
-  preyX = preyX + preyVX;
-  preyY = preyY + preyVY;
+  // Update smurf position based on velocity
+  smurfX = smurfX + smurfVX;
+  smurfY = smurfY + smurfVY;
 
   // Screen wrapping
-  if (preyX < 0) {
-    preyX = preyX + width;
+  if (smurfX < 0) {
+    smurfX = smurfX + width;
   }
-  else if (preyX > width) {
-    preyX = preyX - width;
+  else if (smurfX > width) {
+    smurfX = smurfX - width;
   }
 
-  if (preyY < 0) {
-    preyY = preyY + height;
+  if (smurfY < 0) {
+    smurfY = smurfY + height;
   }
-  else if (preyY > height) {
-    preyY = preyY - height;
+  else if (smurfY > height) {
+    smurfY = smurfY - height;
   }
 }
 
-// drawPrey()
+// drawsmurf()
 //
-// Draw the prey as an ellipse with alpha based on health
-function drawPrey() {
-  fill(preyFill, preyHealth);
-  ellipse(preyX, preyY, preyRadius * 2);
+// Draw the smurf as an ellipse with alpha based on health
+function drawsmurf() {
+  image(smurfImage,smurfX, smurfY, smurfImage.width * smurfscale, smurfImage.height * smurfscale);
 }
 
-// drawPlayer()
+// drawplayerGargamel()
 //
-// Draw the player as an ellipse with alpha value based on health
-function drawPlayer() {
-  fill(playerFill, playerHealth);
-  ellipse(playerX, playerY, playerRadius * 2);
+// Draw the playerGargamel as an ellipse with alpha value based on health
+function drawplayerGargamel() {
+  tint(255, playerGargamelHealth);
+  image(playerGargamelImage,playerGargamelX, playerGargamelY,
+        playerGargamelImage.width * playerGargamelscale, playerGargamelImage.height * playerGargamelscale);
 }
 
 // showGameOver()
@@ -280,8 +293,8 @@ function showGameOver() {
   fill(0);
   // Set up the text to display
   let gameOverText = "GAME OVER\n"; // \n means "new line"
-  gameOverText = gameOverText + "You ate " + preyEaten + " prey\n";
-  gameOverText = gameOverText + "before you died."
+  gameOverText = gameOverText + "You catched " + smurfEaten + " smurf(s)\n";
+  gameOverText = gameOverText + "before you died of hopelessness."
   // Display it in the centre of the screen
   text(gameOverText, width / 2, height / 2);
 }
